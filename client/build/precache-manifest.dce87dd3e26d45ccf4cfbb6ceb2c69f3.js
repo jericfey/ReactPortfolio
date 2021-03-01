df11a68d72d3c8d1865e8c3226058a6e7f0087bb
Jeri Fey <jericfey@gmail.com>
2021-02-28 22:26:34 -0700

    Trying to deploy to gh-pages

---
+++ client/build/precache-manifest.dce87dd3e26d45ccf4cfbb6ceb2c69f3.js
@@ -0,0 +1,70 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "be451c4843006d04a14300eb018e17f0",
+    "url": "/index.html"
+  },
+  {
+    "revision": "9eb5a35f403172f424c2",
+    "url": "/static/css/2.d4a02963.chunk.css"
+  },
+  {
+    "revision": "c61fa2e93e513716b22b",
+    "url": "/static/css/main.d8ccb64f.chunk.css"
+  },
+  {
+    "revision": "9eb5a35f403172f424c2",
+    "url": "/static/js/2.a8eed15f.chunk.js"
+  },
+  {
+    "revision": "653fd80f43f9fef19c1cbf310a702702",
+    "url": "/static/js/2.a8eed15f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c61fa2e93e513716b22b",
+    "url": "/static/js/main.e69d04eb.chunk.js"
+  },
+  {
+    "revision": "d08766e1dd2bf4f6c209",
+    "url": "/static/js/runtime-main.992eac29.js"
+  },
+  {
+    "revision": "99548b51a69138f731e55a5231ae1093",
+    "url": "/static/media/JeriAvatar.99548b51.gif"
+  },
+  {
+    "revision": "75b03458f77d50a17a4de1908fbf7759",
+    "url": "/static/media/Jeri_Fey_2021.75b03458.Pdf"
+  },
+  {
+    "revision": "a91ad097d24828af724d4fee36a063ed",
+    "url": "/static/media/roboto-all-400-normal.a91ad097.woff"
+  },
+  {
+    "revision": "8bb64952764a884d67019b3486296ab9",
+    "url": "/static/media/roboto-cyrillic-400-normal.8bb64952.woff2"
+  },
+  {
+    "revision": "4743c758a952f2bd4a35d4e42afc002b",
+    "url": "/static/media/roboto-cyrillic-ext-400-normal.4743c758.woff2"
+  },
+  {
+    "revision": "c1e9793c84cb26c44ef2a2cf8b6f49ce",
+    "url": "/static/media/roboto-greek-400-normal.c1e9793c.woff2"
+  },
+  {
+    "revision": "182ee6a4872ca8fa78048951b1561a5c",
+    "url": "/static/media/roboto-greek-ext-400-normal.182ee6a4.woff2"
+  },
+  {
+    "revision": "479970ffb74f2117317f9d24d9e317fe",
+    "url": "/static/media/roboto-latin-400-normal.479970ff.woff2"
+  },
+  {
+    "revision": "455200cb007fe1212c668721d827c691",
+    "url": "/static/media/roboto-latin-ext-400-normal.455200cb.woff2"
+  },
+  {
+    "revision": "a8be5b46d06bb541b0968196ee5e6bb8",
+    "url": "/static/media/roboto-vietnamese-400-normal.a8be5b46.woff2"
+  }
+]);
--- client/build/service-worker.js
+++ client/build/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/ReactPortfolio/precache-manifest.4cb8b2055bdb7144624fa23565560887.js"
+  "/precache-manifest.dce87dd3e26d45ccf4cfbb6ceb2c69f3.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/ReactPortfolio/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
